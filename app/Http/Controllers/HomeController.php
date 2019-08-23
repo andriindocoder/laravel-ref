cbc9cdec2a17ccad9eeff657fdcad99bf10389e8
andri <andriwicaksonost@gmail.com>
2019-08-23 11:49:25 +0700

    Change Password
    How to Change Password on Laravel, Change Password Form

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,33 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function showChangePasswordForm(){
+        return view('auth.changepassword');
+    }
+
+    public function changePassword(Request $request){
+     
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+        // The passwords matches
+        return redirect()->back()->with("error","Password Sekarang yang Anda masukkan salah. Silahkan coba lagi.");
+        }
+         
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+        //Current password and new password are same
+        return redirect()->back()->with("error","Password Baru Anda tidak boleh sama dengan Password Sekarang. Silahkan pilih password lainnya.");
+        }
+        if(!(strcmp($request->get('new-password'), $request->get('new-password_confirmation'))) == 0){
+                    //New password and confirm password are not same
+                    return redirect()->back()->with("error","Password Baru harus sesuai dengan Konfirmasi Password. Silahkan coba lagi.");
+        }
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+         
+        return redirect()->back()->with("success","Password berhasil diubah !");
+     
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/change-password', 'HomeController@showChangePasswordForm');
+Route::post('/change-password', 'HomeController@changePassword')->name('change-password');
