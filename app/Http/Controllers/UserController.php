44271ba88a8306ec59ccac86c39261fa045aa7d1
andri <andriwicaksonost@gmail.com>
2019-08-24 20:19:18 +0700

    UserController public function users response()->json()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
-    //
+    public function users(User $user){
+    	$users = $user->all();
+
+    	return response()->json($users);
+    }
 }
