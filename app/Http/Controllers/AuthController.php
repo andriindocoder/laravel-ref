a65082f772f33fac32429d7af8eceed11c38a489
andri <andriwicaksonost@gmail.com>
2019-08-24 20:30:08 +0700

    AuthController this->validate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+    	$this->validate($request, [
+			'name'     => 'required',
+			'email'    => 'required|email|unique:users',
+			'password' => 'required|min:6'
+    	]);
+    }
 }
