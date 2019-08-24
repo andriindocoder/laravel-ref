9bdbed9aaf87c3a4c58e00450e5bcfb53f755c5b
andri <andriwicaksonost@gmail.com>
2019-08-24 20:23:00 +0700

    Fractal config app providers and alias

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Spatie\Fractal\FractalServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Fractal' => Spatie\Fractal\FractalFacade::class,
     ],
 
 ];
