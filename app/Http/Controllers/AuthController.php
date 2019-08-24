198c01952212b871bcd811a7d056ab369bc1bf88
andri <andriwicaksonost@gmail.com>
2019-08-24 20:37:51 +0700

    AuthController public function login
    AuthController public function login
    POST auth/login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Transformers\UserTransformer;
+use Auth;
 
 class AuthController extends Controller
 {
@@ -16,17 +17,30 @@ class AuthController extends Controller
     	]);
 
     	$user = $user->create([
-			'name'      => $request->name,
-			'email'     => $request->email,
-			'password'  => bcrypt($request->password),
-			'api_token' => bcrypt($request->email)
+    		'name' => $request->name,
+    		'email' => $request->email,
+    		'password' => bcrypt($request->password),
+    		'api_token' => bcrypt($request->email)
     	]);
-
+    	
     	$response = fractal()
     		->item($user)
     		->transformWith(new UserTransformer)
     		->toArray();
-
+    	
     	return response()->json($response, 201);
+    }
+
+    public function login(Request $request, User $user){
+        if(!Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            return response()->json(['error' => 'Wrong Credentials'], 401);
+        }
+    
+        $user = $user->find(Auth::user()->id);
+    
+        return fractal()
+            ->item($user)
+            ->transformWith(new UserTransformer)
+            ->toArray();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ use Illuminate\Http\Request;
 
 Route::get('users','UserController@users');
 Route::post('auth/register','AuthController@register');
+Route::post('auth/login','AuthController@login');
