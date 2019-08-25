d33de5adab32652ead03763674239621913dfe38
andri <andriwicaksonost@gmail.com>
2019-08-25 16:08:50 +0700

    Exception error authorization

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($request->expectsJson()){
+            if($exception instanceof \Illuminate\Auth\Access\AuthorizationException){
+                return response()->json([
+                    'error' => 'Unauthorized'
+                ], 401);
+            }
+        }
         return parent::render($request, $exception);
     }
 }
