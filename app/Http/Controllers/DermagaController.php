555573a6bbd07ee91400288e7c50a41df0de7dee
andri <andriwicaksonost@gmail.com>
2019-08-27 17:47:11 +0700

    Retrieve only fraction amongs all the columns

--- app/Http/Controllers/DermagaController.php
+++ app/Http/Controllers/DermagaController.php
@@ -10,7 +10,8 @@ class DermagaController extends Controller
 {
     public function index(){
     	// $dermagas = DB::table('m_dermaga')->get();
-    	$dermaga = DB::table('m_dermaga')->where('kode_pelabuhan','IDJKT')->first();
+    	// $dermaga = DB::table('m_dermaga')->where('kode_pelabuhan','IDJKT')->first();
+    	$dermaga = DB::table('m_dermaga')->where('kode_pelabuhan','IDJKT')->value('nama_pelabuhan');
     	echo "<pre>";
     	print_r($dermaga);
     	echo "</pre>";
