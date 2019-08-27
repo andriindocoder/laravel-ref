118caf270ea2f36c04dee0fda23877fc6c6fd866
andri <andriwicaksonost@gmail.com>
2019-08-27 17:49:38 +0700

    pluck untuk  key value. Keynya yang parameter kedua, value parameter pertama, diambil yang paling akhir

--- app/Http/Controllers/DermagaController.php
+++ app/Http/Controllers/DermagaController.php
@@ -12,7 +12,7 @@ class DermagaController extends Controller
     	// $dermagas = DB::table('m_dermaga')->get();
     	// $dermaga = DB::table('m_dermaga')->where('kode_pelabuhan','IDJKT')->first();
     	// $dermaga = DB::table('m_dermaga')->where('kode_pelabuhan','IDJKT')->value('nama_pelabuhan');
-    	$dermagas = DB::table('m_dermaga')->pluck('kode_pelabuhan');
+    	$dermagas = DB::table('m_dermaga')->pluck('nama_dermaga','kode_dermaga');
     	echo "<pre>";
     	print_r($dermagas);
     	echo "</pre>";
