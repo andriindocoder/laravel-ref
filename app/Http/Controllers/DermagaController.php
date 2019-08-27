f4cb4971c0b8521bd02c27cd37af1df096a25ecb
andri <andriwicaksonost@gmail.com>
2019-08-27 17:51:55 +0700

    chunk, mengambil beberapa record

--- app/Http/Controllers/DermagaController.php
+++ app/Http/Controllers/DermagaController.php
@@ -12,10 +12,12 @@ class DermagaController extends Controller
     	// $dermagas = DB::table('m_dermaga')->get();
     	// $dermaga = DB::table('m_dermaga')->where('kode_pelabuhan','IDJKT')->first();
     	// $dermaga = DB::table('m_dermaga')->where('kode_pelabuhan','IDJKT')->value('nama_pelabuhan');
-    	$dermagas = DB::table('m_dermaga')->pluck('nama_dermaga','kode_dermaga');
-    	echo "<pre>";
-    	print_r($dermagas);
-    	echo "</pre>";
-    	die();
+    	// $dermagas = DB::table('m_dermaga')->pluck('nama_dermaga','kode_dermaga');
+    	DB::table('m_dermaga')->orderBy('id')->chunk(20, function($dermagas){
+	    	echo "<pre>";
+	    	print_r($dermagas);
+	    	echo "</pre>";
+	    	die();
+    	});
     }	
 }
