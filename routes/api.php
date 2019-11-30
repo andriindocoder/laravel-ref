53a5ef9bf177ca1039459906bfc7569c1b1be55d
Andri Sedania <andri@sedaniainnovator.com>
2019-11-30 08:21:28 +0700

    105 - Creating API Endpoint for adding new questions

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,8 @@ Route::get('/questions', 'Api\QuestionsController@index');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:api'])->group(function() {
+	Route::apiResource('/questions', 'Api\QuestionsController')->except('index');
+});
+
