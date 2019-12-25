d67433ecec47f16fb38196fe138601a290d7ffc7
Andri Sedania <andri@sedaniainnovator.com>
2019-12-25 10:52:34 +0700

    6.29 Make Our Global Scopes Reusable

--- laravel6/app/Contact.php
+++ laravel6/app/Contact.php
@@ -5,10 +5,13 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Scopes\FilterScope;
 use App\Scopes\SearchScope;
+use App\Scopes\ContactSearchScope;
 
 class Contact extends Model
 {
     protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'address', 'company_id'];
+
+    public $filterColumns = ['company_id'];
 
     public function company()
     {
@@ -24,6 +27,6 @@ class Contact extends Model
     	parent::boot();
 
     	static::addGlobalScope(new FilterScope);
-    	static::addGlobalScope(new SearchScope);
+    	static::addGlobalScope(new ContactSearchScope);
     }
 }
