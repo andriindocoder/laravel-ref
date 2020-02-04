04434964411aba29ec13bd3316b98fa9a1a231d0
Andri Sedania <andri@sedaniainnovator.com>
2020-02-04 14:29:41 +0700

    10.48 - Resourceful Route

--- laravel6/routes/web.php
+++ laravel6/routes/web.php
@@ -16,19 +16,25 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth', 'verified')->group(function () {
-	Route::get('/contacts', 'ContactController@index')->name('contacts.index');
+	// Route::get('/contacts', 'ContactController@index')->name('contacts.index');
 
-	Route::post('/contacts', 'ContactController@store')->name('contacts.store');
+	// Route::post('/contacts', 'ContactController@store')->name('contacts.store');
 
-	Route::get('/contacts/create', 'ContactController@create')->name('contacts.create');
+	// Route::get('/contacts/create', 'ContactController@create')->name('contacts.create');
 
-	Route::get('/contacts/{contact}', 'ContactController@show')->name('contacts.show');
+	// Route::get('/contacts/{contact}', 'ContactController@show')->name('contacts.show');
 
-	Route::put('/contacts/{contact}', 'ContactController@update')->name('contacts.update');
+	// Route::put('/contacts/{contact}', 'ContactController@update')->name('contacts.update');
 
-	Route::get('/contacts/{contact}/edit', 'ContactController@edit')->name('contacts.edit');
+	// Route::get('/contacts/{contact}/edit', 'ContactController@edit')->name('contacts.edit');
 
-	Route::delete('/contacts/{contact}', 'ContactController@destroy')->name('contacts.destroy');
+	// Route::delete('/contacts/{contact}', 'ContactController@destroy')->name('contacts.destroy');
+	// Route::resource('contacts', 'ContactController')->only(['create', 'store', 'edit', 'update', 'destroy']);
+	// Route::resource('contacts', 'ContactController')->except(['create', 'store', 'edit', 'update', 'destroy']);
+	Route::resources([
+		'/contacts' => 'ContactController',
+		'/companies' => 'CompanyController'
+	]);
 });
 
 Route::get('/settings/account', 'Settings\AccountController@index');
