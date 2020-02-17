e54498751a7c03f70534fed7704c1375e40386c5
Andri Sedania <andri@sedaniainnovator.com>
2020-02-17 20:13:26 +0700

    11.58 - Display All Posts Part 2

--- laravel6/app/User.php
+++ laravel6/app/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable implements MustVerifyEmail
     public function companies() {
         return $this->hasMany(Company::class);
     }
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
 }
