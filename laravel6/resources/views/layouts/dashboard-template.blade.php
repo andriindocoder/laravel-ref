6e80e9544c21b09a921d8a955f60dbd708082f80
Andri Sedania <andri@sedaniainnovator.com>
2020-03-04 13:03:17 +0700

    3.26 - Setup Backend Layout Part 2 (Dashboard)

---
+++ laravel6/resources/views/layouts/dashboard-template.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  @include('layouts.dashboard.head')
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  @include('layouts.dashboard.navbar')
+
+  @include('layouts.dashboard.sidebar')
+
+  @yield('content')
+
+  @include('layouts.dashboard.rightbar')
+
+  @include('layouts.dashboard.footer')
+</div>
+<!-- ./wrapper -->
+@include('layouts.dashboard.script')
+</body>
+</html>
---
+++ laravel6/resources/views/layouts/dashboard/script.blade.php
@@ -0,0 +1,9 @@
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{ asset('dashboard-asset/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('dashboard-asset/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('dashboard-asset/dist/js/adminlte.min.js') }}"></script>
