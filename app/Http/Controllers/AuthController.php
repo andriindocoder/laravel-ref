110d4feadd4c40c52525c93b6ba160abc31d1f59
andri <andriwicaksonost@gmail.com>
2020-06-10 14:16:09 +0700

    16. Membuat Fungsi Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
@@ -35,7 +36,33 @@ class AuthController extends Controller
     }
 
     public function login(Request $request) {
+        $email = $request->input('email');
+        $password = $request->input('password');
 
+        $user = User::where('email', $email)->first();
+
+        if(Hash::check($password, $user->password)) {
+            $apiToken = base64_encode(Str::random(40));
+
+            $user->update([
+                'api_token' => $apiToken
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login Success',
+                'data' => [
+                    'user' => $user,
+                    'api_token' => $apiToken
+                ]
+                ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Failed',
+                'data' => ''
+            ]);
+        }
     }
 
 }
