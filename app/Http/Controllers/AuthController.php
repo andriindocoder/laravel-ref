1395faa99be16e16010855dfe413955496e3fdc7
andri <andriwicaksonost@gmail.com>
2020-06-10 14:08:23 +0700

    15. Membuat Fungsi Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,25 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        // $this->middleware('age', ['only' => ['getUser']]);
-        // $this->middleware('age', ['except' => ['getUser']]);
-    }
-
     public function register(Request $request) {
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = Hash::make($request->input('password'));
 
+        $register = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if($register) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Registration success',
+                'data' => $register
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Registration failed',
+                'data' => ''
+            ], 400);
+        }
     }
 
     public function login(Request $request) {
