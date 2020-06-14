fb8701d8d39d3be755dd2068435c94bd0abab1ad
andri <andriwicaksonost@gmail.com>
2020-06-14 09:26:14 +0700

    02. Membuat Component Livewire

---
+++ app/Http/Livewire/ContactIndex.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Contact;
+
+class ContactIndex extends Component
+{
+	public $data;
+
+    public function render()
+    {
+    	$this->data = Contact::latest()->get();
+        return view('livewire.contact-index');
+    }
+}
---
+++ resources/views/livewire/contact-index.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <ul>
+    	@foreach($data as $contact)
+    	<li>{{ $contact->name }}</li>
+    	@endforeach
+    </ul>
+</div>
