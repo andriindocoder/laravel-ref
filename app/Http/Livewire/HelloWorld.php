14ae20dfb57b0f8c4b82b9cd45c762df5825a921
andri <andriwicaksonost@gmail.com>
2020-06-14 17:04:50 +0700

    Getting Started : Data Binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,10 +6,13 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+	public $name = 'Jelly';
+	public $loud = false;
+	// public $greeting = 'Hello';
+	public $greeting = ['Hello'];
+
     public function render()
     {
-        return view('livewire.hello-world', [
-        	'name' => 'Jolly'
-        ]);
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,22 @@
 <div>
-    Hello {{ $name }}
+    {{-- <input wire:model.debounce.1000ms="name" type="text"> Hello {{ strtoupper($name) }} --}}
+    {{-- <input wire:model.lazy="name" type="text"> Hello {{ strtoupper($name) }} --}}
+    <input wire:model="name" type="text"> 
+
+    <input wire:model="loud" type="checkbox">
+
+    {{-- <select wire:model="greeting">
+    	<option>Hello</option>
+    	<option>Goodbye</option>
+    	<option>Adios</option>
+    </select> --}}
+
+    <select wire:model="greeting" multiple>
+    	<option>Hello</option>
+    	<option>Goodbye</option>
+    	<option>Adios</option>
+    </select>
+
+    {{-- {{ $greeting }} {{ $name }} @if($loud) ! @endif --}}
+    {{ implode(', ', $greeting) }} {{ $name }} @if($loud) ! @endif
 </div>
