46c268b68a156d69f5df4eea0d876a52f25d2647
andri <andriwicaksonost@gmail.com>
2020-06-14 09:31:45 +0700

    03 - Menampilkan Data Contact dalam Table

--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -7,11 +7,10 @@ use App\Contact;
 
 class ContactIndex extends Component
 {
-	public $data;
-
     public function render()
     {
-    	$this->data = Contact::latest()->get();
-        return view('livewire.contact-index');
+        return view('livewire.contact-index', [
+        	'contacts' => Contact::latest()->get()
+        ]);
     }
 }
