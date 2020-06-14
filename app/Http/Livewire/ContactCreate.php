a736e4879ccc7b4a6166246fabc5e29d269fae12
andri <andriwicaksonost@gmail.com>
2020-06-14 09:49:57 +0700

    05. Menyimpan Data dari Form ke Database

--- app/Http/Livewire/ContactCreate.php
+++ app/Http/Livewire/ContactCreate.php
@@ -3,17 +3,33 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Contact;
 
 class ContactCreate extends Component
 {
-	// public $contacts;
-
-	// public function mount($contacts) {
-	// 	$this->contacts = $contacts;
-	// }
+	public $name;
+	public $phone;
 
     public function render()
     {
         return view('livewire.contact-create');
+    }
+
+    public function store() 
+    {
+    	$contact = Contact::create([
+    		'name' => $this->name,
+    		'phone' => $this->phone
+    	]);
+
+    	$this->resetInput();
+
+    	$this->emit('contactStored', $contact);
+    }
+
+    private function resetInput()
+    {
+    	$this->name = null;
+    	$this->phone = null;
     }
 }
--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -7,10 +7,19 @@ use App\Contact;
 
 class ContactIndex extends Component
 {
+
+	protected $listeners = [
+		'contactStored' => 'handleStored'
+	];
+
     public function render()
     {
         return view('livewire.contact-index', [
         	'contacts' => Contact::latest()->get()
         ]);
+    }
+
+    public function handleStored($contact) {
+    	// dd($contact);
     }
 }
