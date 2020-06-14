2016d4013db310faa7cc9672d1355b94e9590676
andri <andriwicaksonost@gmail.com>
2020-06-14 10:29:17 +0700

    12. Membuat Index Paginate Dinamis

--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -15,6 +15,18 @@
 
 	<hr>
 
+    <div class="row">
+        <div class="col">
+            <select wire:model="paginate" name="" id="" class="form-control form-control-sm w-auto">
+                <option value="2">2</option>
+                <option value="5">5</option>
+                <option value="10">10</option>
+            </select>
+        </div>
+    </div>
+
+    <hr>
+
     <table class="table">
     	<thead class="thead-dark">
     		<tr>
