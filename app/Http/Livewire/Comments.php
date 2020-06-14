69b54f6831f04a812b92fa68782e6d9fc2b0f582
andri <andriwicaksonost@gmail.com>
2020-06-14 22:02:46 +0700

    4. Livewire Comments System
    Install tailwind

---
+++ app/Http/Livewire/Comments.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Carbon\Carbon;
+
+class Comments extends Component
+{
+	public $comments = [
+		[
+			'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, vitae, asperiores. Esse amet quibusdam adipisci omnis, molestias accusantium. Natus, non!',
+			'created_at' => '3 min ago',
+			'creator' => 'Sarthak'
+		]
+	];
+
+	public $newComment;
+
+	public function addComment()
+	{
+		array_unshift($this->comments, [
+			'body' => $this->newComment,
+			'created_at' => Carbon::now()->diffForHumans(),
+			'creator' => 'Bitfumes'
+		]);
+
+		$this->newComment = '';
+	}
+
+    public function render()
+    {
+        return view('livewire.comments');
+    }
+}
