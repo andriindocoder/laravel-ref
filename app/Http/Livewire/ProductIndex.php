2ca164e0213aa5410fb5a3bbe0556fce2d6eba76
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-06-21 14:48:57 +0700

    Product search

--- app/Http/Livewire/ProductIndex.php
+++ app/Http/Livewire/ProductIndex.php
@@ -3,11 +3,21 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Product;
+use Livewire\WithPagination;
 
 class ProductIndex extends Component
 {
+	use WithPagination;
+
+	public $search;
+
+	protected $updatesQueryString = ['search'];
+
     public function render()
     {
-        return view('livewire.product-index');
+        return view('livewire.product-index', [
+        	'products' => $this->search === null ? Product::paginate(12) : Product::where('name', 'like', '%' . $this->search . '%')->paginate(12)
+        ]);
     }
 }
--- resources/views/livewire/product-index.blade.php
+++ resources/views/livewire/product-index.blade.php
@@ -1,3 +1,17 @@
-<div>
-    {{-- The whole world belongs to you --}}
+<div class="row">
+	<div class="col-md-12">
+		<input wire:model="search" type="text" placeholder="Search..." class="form-control mb-3">
+	</div>
+    @foreach($products as $product)
+		<div class="col-md-3 mt-2">
+			<div class="card">
+				<div class="card-header">{{ $product->name }}</div>
+				<div class="card-body">{{ $product->description }}</div>
+				<div class="card-footer">
+					<button class="btn btn-success">Add to Cart</button>
+				</div>
+			</div>
+		</div>
+	@endforeach
+	{{ $products->links() }}
 </div>
