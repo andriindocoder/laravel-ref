904aef2d2e3946fc02ebce6d733912c248ab933d
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-06-24 13:25:28 +0700

    Pass book can be checked out

--- app/Book.php
+++ app/Book.php
@@ -8,8 +8,21 @@ class Book extends Model
 {
     protected $guarded = [];
 
+    public function checkout($user) 
+    {
+    	$this->reservations()->create([
+    		'user_id' => $user->id,
+    		'checked_out_at' => now(),
+    	]);
+    }
+
     public function path()
     {
     	return '/books/' . $this->id;
+    }
+
+    public function reservations()
+    {
+    	return $this->hasMany(Reservation::class);
     }
 }
