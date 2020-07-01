21a0338abeeed34a9109fd1925222570857099d7
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-07-01 11:16:09 +0700

    AlpineJS Tutorial - Dropdown
    Toggle Dropdown list
    Press click away isOpen False
    Press escape isOpen false

--- resources/views/alpine.blade.php
+++ resources/views/alpine.blade.php
@@ -9,14 +9,18 @@
                 <ul class="flex">
                     <li><a href="#">Home</a></li>                    
                 </ul>
-                <div class="relative">
+                <div class="relative" x-data="{ isOpen: false }">
                     <button 
+                    	x-on:click = "isOpen = !isOpen"
                         class="flex items-center" 
+                        @keydown.escape="isOpen = false"
                     >
                         <img src="http://www.gravatar.com/avatar?d=mm" alt="avatar" class="w-8 h-8 rounded-full">
                         <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z" class="heroicon-ui"></path></svg>
                     </button>
                     <ul
+                    	x-show="isOpen"
+                    	@click.away="isOpen = false"
                         class="absolute font-normal bg-white shadow overflow-hidden rounded w-48 border mt-2 py-1 right-0 z-20"
                     >
                         <li>
