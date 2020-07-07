87308d83f1691fb2f5cd20442843d09b768548a8
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-07-07 18:04:51 +0700

    Traversy simple flexbox

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::group(['middleware' => 'guest'], function() {
 	Route::livewire('/login', 'login')->name('login');
 	Route::livewire('/register', 'register');
 });
+
+Route::get('/flexbox', function() {
+	return view('flexbox');
+});
