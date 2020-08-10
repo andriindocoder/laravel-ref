b53a03e789a0b631e9e6930e641c0f0b49475007
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-08-10 19:37:49 +0700

    Andre Movie Laravel vid 2-4

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', 'MoviesController@index')->name('movies.index');
+Route::get('/movies/{movie}', 'MoviesController@show')->name('movies.show');
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
 
-Route::view('/tailwind', 'tailwind');
 
-Route::view('/alpine', 'alpine');
+// Route::view('/', 'index');
+// Route::view('/movie', 'show');
+
+// Route::view('/tailwind', 'tailwind');
+
+// Route::view('/alpine', 'alpine');
