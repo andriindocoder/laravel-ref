6492e98584b71f39f07228c8dc2b15b18593a183
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-09-04 18:49:15 +0700

    13. Send Email to Reset Password

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ResetPasswordMail;
 
 class ResetPasswordController extends Controller
 {
-    public function sendEmail()
-    {
-        
+    public function sendEmail(Request $request){
+        if(!$this->validateEmail($request->email)) {
+            return $this->failedResponse();
+        }
+
+        return $this->send($request->email);
+    }
+
+    public function send($email){
+        Mail::to($email)->send(new ResetPasswordMail);
+    }
+
+    public function validateEmail($email) {
+        return !!User::where('email', $email)->first();
+    }
+
+    public function failedResponse() {
+        return response()->json([
+            'error' => 'Email not avaiable on the database'
+        ], Response::HTTP_NOT_FOUND);
     }
 }
